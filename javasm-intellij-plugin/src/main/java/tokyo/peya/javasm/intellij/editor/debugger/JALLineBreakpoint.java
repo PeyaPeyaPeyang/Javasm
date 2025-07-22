2d93fe499bde7c3eb20a47ae06a01c8aef5c5f20
PeyaPeyaPeyang <peyang@peya.tokyo>
2025-07-22 23:51:26 +0900

    feat: Anti-nullpo

--- javasm-intellij-plugin/src/main/java/tokyo/peya/javasm/intellij/editor/debugger/JALLineBreakpoint.java
+++ javasm-intellij-plugin/src/main/java/tokyo/peya/javasm/intellij/editor/debugger/JALLineBreakpoint.java
@@ -12,4 +12,15 @@ public class JALLineBreakpoint extends LineBreakpoint<JavaLineBreakpointProperti
     {
         super(project, xBreakpoint);
     }
+
+    @Override
+    protected @NotNull JavaLineBreakpointProperties getProperties()
+    {
+        // XBreakpoint からとならないとだめ。（super#getProperties() は @NotNull であるから，ぬるぽする。
+        XBreakpoint<JavaLineBreakpointProperties> xBreakpoint = super.getXBreakpoint();
+        JavaLineBreakpointProperties properties = xBreakpoint.getProperties();
+        if (properties == null)
+            properties = new  JavaLineBreakpointProperties();
+        return properties;
+    }
 }
